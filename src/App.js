bda3412bf0fbe654166be8360580f8bf3a44f001
Mvahedii <vahedi.ce@gmail.com>
2019-05-06 10:41:26 -0700

    Initial Commit for Login Component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
-import React from "react";import { Route, BrowserRouter as Router } from "react-router-dom";import Dashboard from "./Component/Dashboard/index";import PrivateRoute from './Component/PrivateRoutes'function App() {  return (    <Router>      <Route exact path="/login" />      <PrivateRoute exact path='/dashboard' component={Dashboard} />    </Router>  )}export default App;
+import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Dashboard from "./Component/Dashboard/index";
+import PrivateRoute from './Component/Utilities/PrivateRoutes'
+import Login from './Component/Login/index'
+
+function App() {
+
+
+  return (
+    <Router>
+      <Route exact path="/login" component={Login} />
+      <PrivateRoute exact path='/dashboard' component={Dashboard} />
+    </Router>
+  )
+
+}
+
+
+export default App;
