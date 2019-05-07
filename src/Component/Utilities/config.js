39e185b8e69db84ba886c4e38d1d4e85cd287a01
nima habibkhoda <nima.2004hkh@gmail.com>
2019-05-07 09:40:22 +0430

    Add login error

--- src/Component/Utilities/config.js
+++ src/Component/Utilities/config.js
@@ -3,3 +3,7 @@ const server="http://shop.nimahabibkhoda.ir/api/v1/"
 export const Routers = {
     "Login_API":server+"admin/login"
 }
+
+export const messages={
+    "Login_Alert":"خطا در ارسال داده ها"
+}
